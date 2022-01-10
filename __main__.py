a213b0ff90e15654cf2c8fd60d72f7718c01d950
pome-ta <pome-ta@users.noreply.github.com>
2022-01-10 17:20:19 +0900

    add pythonista

---
+++ __main__.py
@@ -0,0 +1,27 @@
+import sys
+import pathlib
+import ui
+
+sys.path.append(str(pathlib.Path.cwd()) + '/pythonista-webview')
+import wkwebview
+
+uri = pathlib.Path('./public/index.html')
+
+
+class View(ui.View):
+  def __init__(self, *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
+    self.wv = wkwebview.WKWebView()
+    self.wv.load_url(str(uri), True)
+    self.wv.flex = 'WH'
+    self.add_subview(self.wv)
+
+  def will_close(self):
+    self.wv.clear_cache()
+
+
+if __name__ == '__main__':
+  view = View()
+  view.present(style='fullscreen', orientations=['portrait'])
+  view.wv.clear_cache()
+
