7c1649b3c0aa9fab5516b08b2f2c8a6dbe851afd
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-02-04 17:50:41 +0900

    bass

--- public/shader/sound.py
+++ public/shader/sound.py
@@ -9,7 +9,7 @@ float timeToBeat(float t) {return t / 60.0 * BPM;}
 float beatToTime(float b) {return b / BPM * 60.0;}
 
 float sine(float phase) {
-  return sin(TAU * phase );
+  return sin(TAU * phase);
 }
 float saw(float phase) {
   return 2.0 * fract(phase) - 1.0;
@@ -24,15 +24,9 @@ float pulse(float phase, float duty) {
   return fract(phase) < abs(duty) ? -1.0 : 1.0;
 }
 
-
-
-
-
-float adsr(float t, float a, float d, float s, float r, float gt) {
-  return max(0.0, min(1.0, t/max(1e-4, a)) - min((1.0 - s) ,max(0.0, t - a)*(1.0 - s)/max(1e-4, d)) - max(0.0, t - gt)*s/max(1e-4, r));
+float cent(float dig) {
+  return pow(2.0, dig / 12.0);
 }
-
-
 
 
 
@@ -40,14 +34,23 @@ vec2 mainSound(float time) {
   float beat = timeToBeat(time);
   float tempo = sine((mod(beat, 4.0) >= 1.0 ? 440.0 : 880.0) * time) * exp(-1e2 * fract(beat));
   
-  float m = mod(beat, 1.0);
-  float v = fract(beat / 4.0);
-  float w = sine(44.0 * time);
+  float baseHZ = 55.0;
   
-  float mono = w + v;
+  float gate = mod(beat * 2.0, 8.0);
+  float tone = gate <= 1.0 ? baseHZ * cent(12.0):
+                 gate <= 1.8 ? 0.0:
+               gate <= 2.0 ? baseHZ * cent(12.0):
+                 gate <= 2.8 ? 0.0:
+               gate <= 3.0 ? baseHZ * cent(12.0):
+               gate <= 4.0 ? baseHZ * cent(12.0):
+               gate <= 5.0 ? baseHZ * cent(12.0):
+               gate <= 6.0 ? baseHZ * cent(12.0):
+                 gate <= 6.8 ? 0.0:
+               gate <= 7.0 ? baseHZ * cent(7.0):
+               gate < 8.0 ? 0.0: 0.0;
   
-  //return vec2(clamp(m, -1.0, 1.0), tempo);
-  return vec2(clamp(w * 32.0, -1.0, 1.0) / 2.0);
+  float w = sine(tone * time);
+  return vec2(w, tempo);
 }
 
 
