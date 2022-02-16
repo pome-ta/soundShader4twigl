5f6b088145d282e19d34e15117926ad0a33f1d69
pome-ta <pometarou93@gmail.com>
2022-02-16 15:21:54 +0900

    メトロノーム

--- public/shader/220126_0039.py
+++ public/shader/220126_0039.py
@@ -1,20 +1,4 @@
-/*vec2 mainSound(float time) {
-  float pi = acos(-1.0);
-  float pi2 = pi * 2.0;
-  
-  float tempo = abs(sin(time));
-  
-  float wave44 = sin(pi2 * 440.0 * time);
-  //float env44 = wave44 * mod(-time * 0.5, 0.25);
-  
-  float env44 = wave44 * step(0.1, step(0.9,fract(-1.0 * time)));
-  
-  float wave88 = sin(pi2 * 880.0 * time);
-  float env88 = wave88 * fract(time / -4.0);
-  
-  return vec2(env44, env88);
-}*/
-
+/* メトロノーム */
 const float BPM = 90.0;
 
 float timeToBeat(float time) {
