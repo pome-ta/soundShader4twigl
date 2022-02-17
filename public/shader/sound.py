c3e7b4bdb348b1b1ae9cefb50129ee70adf2ca22
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-02-18 00:44:35 +0900

    up

--- public/shader/sound.py
+++ public/shader/sound.py
@@ -1,42 +1,47 @@
-#define BPM 132.0
+#define BPM 110.0
 const float PI = acos(-1.0);
 const float TAU = PI * 2.0;
 
-
+/* sound common */
 float timeToBeat(float t) {return t / 60.0 * BPM;}
 float beatToTime(float b) {return b / BPM * 60.0;}
 
 
-float saw(float phase) {return 2.0 * fract(phase) - 1.0;}
-float square(float phase) {return fract(phase) < 0.5 ? -1.0 : 1.0;}
-float triangle(float phase) {return 1.0 - 4.0 * abs(fract(phase) - 0.5);}
-float sine(float phase) {return sin(TAU * phase );}
-
-
-float kick(float time) {
-  float amp = exp(-24.0 * time);
-  float phase = 48.0 * time - 4.8 * exp(-48.0 * time);
-  return amp * sine( phase );
+float sine(float phase) {
+  return sin(TAU * phase);
 }
 
 
-vec2 mainSound(float time) {
-  float beat = timeToBeat(time);
-  float tempo = sine((mod(beat, 4.0) >= 1.0 ? 440.0 : 880.0) * time) * exp(-1e2 * fract(beat));
+float pitch(float p, float t) {
+  return pow(2.0, p / 12.0) * t;
+}
+
+float saw(float phase) {
+  float s = 0.0;
+  for (int k=1; k<=8; k++) {
+    s += (sin(TAU * float(k) * phase) / float(k));
+  }
+  return (1.0 / 2.0) - (1.0 / PI) * s - 0.5;
+}
+
+float square(float phase) {
+  float s = 0.0;
+  for (int k=1; k<8; k++) {
+    s += sin(TAU * (2.0 * float(k) - 1.0) * phase) / (2.0 * float(k) - 1.0);
+  }
+  return (4.0 / PI) * s;
+}
+
+float kick_sine(float phase) {
+  return cos(TAU * phase);
+}
+
+vec2 mainSound(float time){
+  float bpm = timeToBeat(time);
+  float tempo = sine((mod(bpm, 4.0) >= 1.0 ? 440.0 : 880.0) * time) * exp(-1e2 * fract(bpm));
   
-  float kikTiming = mod(beat, 16.0) <= 15.0 ? mod(beat, 1.0) : mod(beat, 0.5);
-  float kickTime = beatToTime(kikTiming);
+  float s = kick_sine(pitch(0.0, 64.0) * time) * fract(-bpm);
   
-  float bd = kick(kickTime);
-  
-  
-  float a2 = 110.0;
-  float a3 = 220.0;
-  
-  float w = sine(a3 * time);
-  
-  //return vec2(tempo, bd);
-  //return vec2(bd+ wav);
-  return vec2(bd, w);
+  return vec2(s);
 }
 
