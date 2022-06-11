7a02bfb8ea807bc1804437a49f4b8f750f6eb9dc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-12 00:03:45 +0900

    up

--- public/shader/sound.py
+++ public/shader/sound.py
@@ -1,14 +1,11 @@
 // [20220609_fract step velocity](https://www.shadertoy.com/view/NsVcDR)
 
 
-#define saturate(i) clamp(i,0.,1.)
 #define clip(i) clamp(i,-1.,1.)
-#define lofi(i,j) (floor((i)/(j))*(j))
-#define tri(p) (1.-4.*abs(fract(p)-0.5))
 
 const float BPM = 140.0;
 
-// fract( a * step + b )
+// fract(a * step + b)
 const float FRACT_STEP_VELOCITY_A = 0.62;
 const float FRACT_STEP_VELOCITY_B = 0.67;
 
@@ -31,39 +28,37 @@ float rand(vec2 st) {
 
 
 vec2 kick( float t ) {
-  float phase = 45.0 * t - 6.0 * exp( -40.0 * t ) - 3.0 * exp( -400.0 * t );
-  float decay = exp( -3.0 * t );
-  return vec2( decay * sin( TAU * phase ) );
+  float phase = 45.0 * t - 6.0 * exp(-40.0 * t) - 3.0 * exp(-400.0 * t);
+  float decay = exp(-3.0 * t);
+  return vec2(decay * sin(TAU * phase));
 }
 
 vec2 hihat( float t, float d ) {
-  float decay = exp( -d * t );
-  vec2 sig = vec2(1.0 - 2.0 * rand(vec2(t)));
-  sig -= vec2(1.0 - 2.0 * rand(vec2(t + 0.007))); // pseudo high pass. shoutouts to aaaidan
+  float decay = exp(-d * t);
+  //vec2 sig = vec2(1.0 - 2.0 * rand(vec2(t)));
+  vec2 sig = vec2(1.0 - 2.0 * rand(vec2(140.0, 136.0) * t));
+  // pseudo high pass. shoutouts to aaaidan
+  //sig -= vec2(1.0 - 2.0 * rand(vec2(t + 0.1)));
+  sig -= vec2(1.0 - 2.0 * rand(vec2(140.0, 136.0) * t + 0.007));
   return sig * decay;
 }
 
 
 vec2 mainSound(float time) {
   float beat = time * TIME2BEAT;
-  vec2 dest = vec2( 0.0 );
+  vec2 dest = vec2(0.0);
     
-  float tKick = mod( beat, 1.0 ) * BEAT2TIME;
-  dest += 0.5 * kick( tKick );
+  float tKick = mod(beat, 1.0) * BEAT2TIME;
+  dest += 0.5 * kick(tKick);
   
-  float tHihat = mod( beat, 0.25 ) * BEAT2TIME;
-  float stepHihat = mod( floor( beat * 4.0 ), 16.0 );
-  float velHihat = fract( FRACT_STEP_VELOCITY_A * stepHihat + FRACT_STEP_VELOCITY_B ); // fract step velocity here
-  float decayHihat = pow( 2.0, 8.0 - 3.0 * velHihat );
-  float ampHihat = mix( 0.7, 1.0, velHihat );
-  dest += 0.2 * ampHihat * hihat( tHihat, decayHihat  );
-
-  
-  
+  float tHihat = mod(beat, 0.25) * BEAT2TIME;
+  float stepHihat = mod(floor(beat * 4.0), 16.0);
+  // fract step velocity here
+  float velHihat = fract(FRACT_STEP_VELOCITY_A * stepHihat + FRACT_STEP_VELOCITY_B);
+  float decayHihat = pow(2.0, 8.0 - 3.0 * velHihat);
+  float ampHihat = mix(0.7, 1.0, velHihat);
+  dest += 0.2 * ampHihat * hihat(tHihat, decayHihat);
   
   return clip( dest );
-  //return dest;
 }
-
-
 
