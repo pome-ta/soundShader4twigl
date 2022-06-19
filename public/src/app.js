f63ae584998afc0208294b7d0c127370c00acb5d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-06-19 22:02:14 +0900

    up

--- public/src/app.js
+++ public/src/app.js
@@ -4,7 +4,7 @@ import { barVisualize } from './visualizar.js';
 let VERTEX_SHADER_SOURCE;
 let FRAGMENT_SHADER_SOURCE_HEADER;
 let FRAGMENT_SHADER_SOURCE_FOOTER;
-const DURATION = 16;
+const DURATION = 1;
 const BUFFER_WIDTH = 512;
 const BUFFER_HEIGHT = 512;
 const FFT_SIZE = 128;
