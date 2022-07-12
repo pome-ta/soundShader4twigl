c89f96a002ef8f6839dc4ea041a46eeb7f62c380
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-12 23:49:41 +0900

    220712_2345.py

--- public/shader/sound.py
+++ public/shader/sound.py
@@ -1,64 +1,15 @@
-// [20220609_fract step velocity](https://www.shadertoy.com/view/NsVcDR)
+// [twigl.app](https://twigl.app/?ol=true&mode=3&source=gl_FragColor%3Dvec4(0)%3B&sound=true&soundsource=vec2%20s(float%20t,float%20n)%7B%0A%20%20float%20o%20%3D%201.059%3B%0A%20%20return%20vec2(sin(6.2831*440.*max(0.,t)*pow(o,n))*exp(-3.*t))*0.2%3B%0A%7D%0A%0Avec2%20mainSound(float%20time)%7B%0A%20%20float%20t%3Dtime%3B%0A%20%20return%20s(t-0.,3.)%2Bs(t-1.,5.)%2Bs(t-2.,7.)%3B%0A%7D)
+// [https://twitter.com/ayano_tft/status/1546786874181980160?s=12&t=iIzCEjt5xT2arCDwEDBQ6Q](https://twitter.com/ayano_tft/status/1546786874181980160?s=12&t=iIzCEjt5xT2arCDwEDBQ6Q)
 
 
-#define clip(i) clamp(i,-1.,1.)
 
-const float BPM = 140.0;
-
-// fract(a * step + b)
-const float FRACT_STEP_VELOCITY_A = 0.62;
-const float FRACT_STEP_VELOCITY_B = 0.67;
-
-// constants
-const float PI = acos(-1.0);
-const float TAU = PI * 2.0;
-//const float SQRT2 = sqrt(2.0);
-
-const float BPS = BPM / 60.0;
-const float TIME2BEAT = BPS;
-const float BEAT2TIME = 1.0 / BPS;
-
-
-float rand(vec2 st) {
-  vec2 magic2 = vec2(12.9898, 78.233);
-  float _rnd = sin(dot(st, magic2));
-  return fract(_rnd * 43758.5453123);
+vec2 s(float t,float n){
+  float o = 1.059;
+  return vec2(sin(6.2831*440.*max(0.,t)*pow(o,n))*exp(-3.*t))*0.2;
 }
 
-
-
-vec2 kick( float t ) {
-  float phase = 20.0 * t - 6.0 * exp(-40.0 * t) - 3.0 * exp(-400.0 * t);
-  float decay = exp(-3.0 * t);
-  return vec2(decay * sin(TAU * phase));
-}
-
-vec2 hihat( float t, float d ) {
-  float decay = exp(-d * t);
-  vec2 sig = vec2(1.0 - 2.0 * rand(vec2(t)));
-  //vec2 sig = vec2(1.0 - 2.0 * rand(vec2(140.0, 136.0) * t));
-  // pseudo high pass. shoutouts to aaaidan
-  sig -= vec2(1.0 - 2.0 * rand(vec2(t + 0.007)));
-  //sig -= vec2(1.0 - 2.0 * rand(vec2(140.0, 136.0) * t + 0.007));
-  return sig * decay;
-}
-
-
-vec2 mainSound(float time) {
-  float beat = time * TIME2BEAT;
-  vec2 dest = vec2(0.0);
-    
-  float tKick = mod(beat, 1.0) * BEAT2TIME;
-  dest += 0.5 * kick(tKick);
-  
-  float tHihat = mod(beat, 0.25) * BEAT2TIME;
-  float stepHihat = mod(floor(beat * 4.0), 16.0);
-  // fract step velocity here
-  float velHihat = fract(FRACT_STEP_VELOCITY_A * stepHihat + FRACT_STEP_VELOCITY_B);
-  float decayHihat = pow(2.0, 8.0 - 3.0 * velHihat);
-  float ampHihat = mix(0.7, 1.0, velHihat);
-  dest += 0.2 * ampHihat * hihat(tHihat, decayHihat);
-  
-  return clip( dest );
+vec2 mainSound(float time){
+  float t=time;
+  return s(t-0.,3.)+s(t-1.,5.)+s(t-2.,7.);
 }
 
