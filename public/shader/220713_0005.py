fcdad3203b6fdacdd91c9af513d00fbefb20c80f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-07-13 00:08:15 +0900

    220713_0005.py

--- public/shader/220713_0005.py
+++ public/shader/220713_0005.py
@@ -2,13 +2,13 @@
 // [https://twitter.com/ayano_tft/status/1546786874181980160?s=12&t=iIzCEjt5xT2arCDwEDBQ6Q](https://twitter.com/ayano_tft/status/1546786874181980160?s=12&t=iIzCEjt5xT2arCDwEDBQ6Q)
 
 
-vec2 s(float t, float n){
+vec2 s(float t,float n){
   float o = 1.059;
-  return vec2(sin(6.2831 * 440.0 * max(0.0, t) * pow(o, n)) * exp(-3.0 * t)) * 0.2;
+  return vec2(sin(6.2831*440.*max(0.,t)*pow(o,n))*exp(-3.*t))*0.2;
 }
 
 vec2 mainSound(float time){
-  float t = time;
-  return s(t - 0.0, 3.0) + s(t - 1.0, 5.0) + s(t - 2.0, 7.0);
+  float t=time;
+  return s(t-0.,3.)+s(t-1.,5.)+s(t-2.,7.);
 }
 
